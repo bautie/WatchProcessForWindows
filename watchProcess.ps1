bc7ee81275d3bc5b573e43bd77b99bb890e8b437
bautie <36556238+bautie@users.noreply.github.com>
2019-06-29 18:14:43 +0900

    Add watching multi-process funciton

--- watchProcess.ps1
+++ watchProcess.ps1
@@ -2,26 +2,30 @@
 #  pythonのプロセス数が変化したときに音を鳴らす
 #  powershell.exe -ExecutionPolicy RemoteSigned .\watchProcess.ps1
 
+$targets = "python", "ffmpeg"
+
 $numProcs = 0;
 
-while( $true ) {
 
-    $procs = Get-Process -Name "python" -ErrorAction SilentlyContinue
+while( $true ) {
 
     $lastNumProcs = $numProcs;
     $numProcs = 0;
 
-    foreach( $proc in $procs ) {
-        $numProcs ++
+    foreach( $target in $targets ) {
+        $procs = Get-Process -Name $target -ErrorAction SilentlyContinue
+        foreach( $proc in $procs ) {
+            $numProcs ++
+        }
     }
 
     if ( $lastNumProcs -lt $numProcs ) { # Start
-        [Console]::Beep( 400, 150 )
-        [Console]::Beep( 400, 150 )
+        [Console]::Beep( 400, 50 )
+        [Console]::Beep( 400, 50 )
     } elseif ( $lastNumProcs -gt $numProcs ) { # End
-        [Console]::Beep( 1700, 300 )
-        [Console]::Beep( 1800, 300 )
-        [Console]::Beep( 1900, 300 )
+        [Console]::Beep( 1500, 200 )
+        [Console]::Beep( 1800, 200 )
+        [Console]::Beep( 1900, 200 )
     }
 
     Write-Host -NoNewline " :" $numProcs
